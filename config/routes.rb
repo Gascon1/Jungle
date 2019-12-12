9a240dce5a7da793b941cddc2299d451508989e0
Mikel Gascon <mikelgascon@outlook.com>
2019-12-12 17:15:49 +0000

    implements user auth, lets users create an acccount, displays the name of the user when they are signed in

--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,17 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
-  resource :about, only: [:show]
+  # resource :about, only: [:show]
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+  get '/about' => 'about#show'
   
 
-  # get '/about' => 'about#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
