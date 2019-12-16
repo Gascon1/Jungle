a39873bef83f7e7c2f5c4316989cf91992f003a0
Mikel Gascon <mikelgascon@outlook.com>
2019-12-16 18:55:10 +0000

    implements tests for the product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'should have a name, price, quantity, and category' do
+      @category = Category.new(:name => 'Food')
+      @product = Product.new(:name => 'balls', :price => 2000, :quantity => 32, :category => @category)
+
+      expect(@product.name).to be_present
+      expect(@product.price).to be_present
+      expect(@product.quantity).to be_present
+      expect(@product.category).to be_present
+    end
+
+    it 'should display an error message if name is nil' do
+      @category = Category.new(:name => 'Food')
+      @product = Product.new(
+        :name => nil, 
+        :price => 2000, 
+        :quantity => 32, 
+        :category => @category)
+
+      @product.save
+      
+      expect(@product.errors.full_messages).to eql(["Name can't be blank"])
+
+    end
+
+    it 'should display an error message if price is nil' do
+      @category = Category.new(:name => 'Food')
+      @product = Product.new(
+        :name => 'balls', 
+        :price => nil, 
+        :quantity => 32, 
+        :category => @category)
+
+      @product.save
+      
+      expect(@product.errors.full_messages).to eql(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
+
+    end
+    
+    it 'should display an error message if quantity is nil' do
+      @category = Category.new(:name => 'Food')
+      @product = Product.new(
+        :name => 'balls', 
+        :price => 2000, 
+        :quantity => nil, 
+        :category => @category)
+
+      @product.save
+      
+      expect(@product.errors.full_messages).to eql(["Quantity can't be blank"])
+
+    end
+    
+    it 'should display an error message if category is nil' do
+      @category = Category.new(:name => 'Food')
+      @product = Product.new(
+        :name => 'balls', 
+        :price => 2000, 
+        :quantity => 32, 
+        :category => nil)
+
+      @product.save
+      
+      expect(@product.errors.full_messages).to eql(["Category can't be blank"])
+
+    end
+  end
+end
